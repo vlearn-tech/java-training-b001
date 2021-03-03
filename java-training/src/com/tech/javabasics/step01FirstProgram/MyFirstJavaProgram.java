5fabff5f151a82a3d68921b0e7dbc2bf6f168236
vlearn-tech <vlearn.tech.official@gmail.com>
2021-03-03 14:56:26 +0530

    first commit

---
+++ java-training/src/com/tech/javabasics/step01FirstProgram/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+package com.tech.javabasics.step01FirstProgram;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ java-training/src/com/tech/javabasics/step02ConsolePrinting/ConsolePrinting.java
@@ -0,0 +1,27 @@
+package com.tech.javabasics.step02ConsolePrinting;
+
+public class ConsolePrinting {
+
+	public static void main(String[] args) {
+		System.out.println("I am learning Java");
+		System.out.print("I am finding it difficult at start");
+		System.out.println("..but as I learn more, it will start getting easy.");
+
+		System.out.println("\n\n===============================\n\n");
+
+		// Initialize some variables
+		String name = "ABC";
+		int age = 25;
+
+		System.out.println("Name is " + name);
+		System.out.printf("Name is %s", name).println();
+		System.out.println(String.format("Name is %s", name));
+		System.out.println(String.format("Name is %s", name));
+
+//		3 different ways to print: Name is ABC and age is 25
+		System.out.println("Name is " + name + " and age is " + age);
+		System.out.format("Name is %s and age is %d", name, age);
+		System.out.println(String.format("Name is %s and age is %d", name, age));
+
+	}
+}
