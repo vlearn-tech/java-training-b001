af55b6c5c4b81b47ac6e0fdd98043e0cd9837698
vlearn-tech <vlearn.tech.official@gmail.com>
2021-03-30 09:58:41 +0530

    Reference Types

---
+++ java-training/src/com/tech/javabasics/step10ReferenceTypes/WrapperClass.java
@@ -0,0 +1,19 @@
+package com.tech.javabasics.step10ReferenceTypes;
+
+@SuppressWarnings("unused")
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		Integer number = 10;
+		Integer anotherNumber = Integer.valueOf(20);
+
+		System.out.println("Max range of Integer is : " + Integer.MAX_VALUE);
+		System.out.println("Min range of Integer is : " + Integer.MIN_VALUE);
+
+		String scannedValue = "12034";
+		Integer scannedNumberValue = Integer.valueOf(scannedValue);
+
+		System.out.println("Next value of the scanned number is : " + ++scannedNumberValue);
+	}
+
+}
